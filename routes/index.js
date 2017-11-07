ae114f1681d7692da5f4f0aacb59735715c8299b
Bryce Baker <baker.b.rice@gmail.com>
2017-11-06 22:38:05 -0800

    good finally 3?

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,10 @@ var router = express.Router();
 //########################################
 //to process data sent in on request need body-parser module
 var bodyParser = require('body-parser');
-
-router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 //#########################################
 
-router.post('/getMyNodeResults', function(req, res) {
+router.post('/getMyNodeResults', function(req, res, next) {
     //expecting data variable called name --retrieve value using body-parser
-    //var value_name = req.body.name;  //retrieve the data associated with name
-    res.send("hello ");
+    var value_name = req.body.name;  //retrieve the data associated with name
+    res.send("hello " + value_name);
 });
